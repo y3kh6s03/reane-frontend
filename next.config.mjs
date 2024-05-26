a54c1cdeb01f95559c141ac66f0d66d5b7882f2e
yosuke026 <y3kh6s03@gmail.com>
2024-05-26 16:48:40 +0900

    next.config.mjs修正

--- next.config.mjs
+++ next.config.mjs
@@ -15,7 +15,7 @@ const nextConfig = {
     ],
   },
   webpack(config) {
-    config.resolve.alias['@'] = path.resolve(process.cwd(), 'frontend/src');
+    config.resolve.alias['@'] = path.resolve(process.cwd(), 'src');
     return config;
   },
 };
