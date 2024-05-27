22dc970832b005e9dc8849f54e4deda6f6cb591a
yosuke026 <y3kh6s03@gmail.com>
2024-05-27 10:05:20 +0900

    環境変数修正のため

--- src/app/components/layouts/nav/Nav.tsx
+++ src/app/components/layouts/nav/Nav.tsx
@@ -3,7 +3,7 @@ import React from 'react';
 import Link from "next/link";
 import Image from "next/image";
 
-import styles from "./nav.module.scss"
+import styles from "./nav.module.scss";
 import Logout from '../../elements/logout/Logout';
 
 interface Props {
