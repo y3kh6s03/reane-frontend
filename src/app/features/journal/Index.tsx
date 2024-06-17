a6af494f5204b8b2b0d391a0257ffa487348535e
yosuke026 <y3kh6s03@gmail.com>
2024-06-17 18:04:41 +0900

    スキルの追加機能を実装

--- src/app/features/journal/Index.tsx
+++ src/app/features/journal/Index.tsx
@@ -1,7 +1,7 @@
 "use client"
 
 import { useState } from "react"
-import { ModalToggleButton } from "@/../app/components/elements/button/Button"
+// import { ModalToggleButton } from "@/../app/components/elements/button/Button"
 import ModalContainer from "@/components/utils/ModalContainer"
 import JournalInputModal from "@/../app/components/elements/Modal/JournalInputModal"
 import styles from "./styles/Journal.module.scss"
@@ -10,16 +10,16 @@ import Archive from "./Archive"
 
 export default function JournalIndex() {
   const [isJournalModal, setIsJournalModal] = useState(false)
-  const modalToggleProps = {
-    setIsModal: setIsJournalModal,
-    toggleName: '振り返り'
-  }
+  // const modalToggleProps = {
+  //   setIsModal: setIsJournalModal,
+  //   toggleName: '振り返り'
+  // }
   return (
     <div id="journal" className={styles.container}>
       <h1 className={styles.page_title}>
         Journal
       </h1>
-      <ModalToggleButton modalToggleProps={modalToggleProps} />
+      {/* <ModalToggleButton modalToggleProps={modalToggleProps} /> */}
 
       <div className={styles.searchContainer}>
         <SearchFilter searchTitle='Reach' searchName='WEB開発者' />
