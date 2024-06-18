d3a983d36e412bcffd3b3a96c084f904d9f9c396
yosuke026 <y3kh6s03@gmail.com>
2024-06-18 15:56:22 +0900

    チャート削除機能の実装

--- src/app/features/chartView/handlers/ReachHandler.ts
+++ src/app/features/chartView/handlers/ReachHandler.ts
@@ -1,6 +1,8 @@
 /* eslint-disable import/prefer-default-export */
 import axios from "axios";
 import { Dispatch, FormEvent, SetStateAction } from "react";
+import { deleteChart } from "../../../../store/slice/AuthChartsSlice";
+import { AppDispatch } from "../../../../store/store";
 
 export interface ReachData {
   id: number | undefined,
@@ -13,11 +15,20 @@ export interface ReachData {
 interface HandleReachNameSubmitProps {
   e: FormEvent<HTMLFormElement>,
   reachData: ReachData,
-  setReachName: Dispatch<SetStateAction<string>>
+  setReachName: Dispatch<SetStateAction<string>>,
+  setErrorMsg: Dispatch<SetStateAction<string | null>>
 }
 
-export const handleReachNameSubmit = async ({ e, reachData, setReachName }: HandleReachNameSubmitProps) => {
+interface HandleReachDeleteSubmitProps {
+  id: number,
+  userEmail: string,
+  dispatch: AppDispatch,
+  setErrorMsg: Dispatch<SetStateAction<string | null>>
+}
+
+export const handleReachNameSubmit = async ({ e, reachData, setReachName, setErrorMsg }: HandleReachNameSubmitProps) => {
   e.preventDefault();
+  setErrorMsg('');
   const form = new FormData(e.currentTarget);
   const editReachName = form.get('reachName');
   if (editReachName !== reachData.name) {
@@ -32,8 +43,30 @@ export const handleReachNameSubmit = async ({ e, reachData, setReachName }: Hand
       const data = await res.data;
       setReachName(data.reach.name);
     } catch (error) {
-      console.error('Failed to update reach name:', error);
+      setErrorMsg('Reachの変更に失敗しました。ï');
     }
+  }
+}
 
+export const handleReachDeleteSubmit = async ({ id, userEmail, dispatch, setErrorMsg }: HandleReachDeleteSubmitProps) => {
+  const result = confirm('チャートを削除しますか？登録されているSkill, Action, 振り返りすべてが削除されます。');
+  if (result) {
+    const finalResult = confirm('こちらのチャートを本当に削除します。');
+    setErrorMsg('');
+    if (finalResult) {
+      try {
+        const URL = `${process.env.NEXT_PUBLIC_BASE_URL}/api/myChart/reach/${id}`;
+        const reachDeletePayload = {
+          id,
+          userEmail
+        }
+        const res = await axios.delete(URL, { data: reachDeletePayload });
+        if (res.status === 200) {
+          dispatch(deleteChart(id));
+        }
+      } catch (error) {
+        setErrorMsg(`チャートの削除に失敗しました。`)
+      }
+    }
   }
 }
