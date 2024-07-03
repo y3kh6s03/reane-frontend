fe8ed7993f9ee2c5c787ee5a2cfc3bd576afe49a
yosuke026 <y3kh6s03@gmail.com>
2024-07-03 23:01:07 +0900

    APIルートのエンドポイント修正

--- src/store/thunks/AllUsersChartsThunk.ts
+++ src/store/thunks/AllUsersChartsThunk.ts
@@ -6,7 +6,7 @@ import { ChartData } from "../slice/AuthChartsSlice";
 export const fetchAllUsersCharts = createAsyncThunk<{ [key: number]: ChartData }>(
   'allUsersCharts/fetchAllUsersCharts',
   async () => {
-    const res = await apiRouteAxios('/api/chart/all')
+    const res = await apiRouteAxios.get('api/chart/all')
     const data = await res.data;
     return data
   }
