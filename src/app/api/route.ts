df4fdb6bb7e125e4e598659b5dba3c02fae6cbb7
yosuke026 <y3kh6s03@gmail.com>
2024-07-04 08:46:03 +0900

    APIルートにルート設定

---
+++ src/app/api/route.ts
@@ -0,0 +1,8 @@
+/* eslint-disable import/prefer-default-export */
+import { backendApiAxios } from "./utils";
+
+export async function GET() {
+  const res = await backendApiAxios.get("/chart/all");
+  const data = await res.data;
+  return Response.json(data);
+}
