40f6fdc2cc63f1eb62edd256330f0a8857094e8e
yosuke026 <y3kh6s03@gmail.com>
2024-07-03 23:06:47 +0900

    APIルートのエンドポイント確認

--- src/store/thunks/AllUsersChartsThunk.ts
+++ src/store/thunks/AllUsersChartsThunk.ts
@@ -1,12 +1,14 @@
 /* eslint-disable import/prefer-default-export */
+import axios from "axios";
 import { createAsyncThunk } from "@reduxjs/toolkit";
-import { apiRouteAxios } from "@/components/utils/apiRouteAxios";
+// import { apiRouteAxios } from "@/components/utils/apiRouteAxios";
 import { ChartData } from "../slice/AuthChartsSlice";
 
 export const fetchAllUsersCharts = createAsyncThunk<{ [key: number]: ChartData }>(
   'allUsersCharts/fetchAllUsersCharts',
   async () => {
-    const res = await apiRouteAxios.get('api/chart/all')
+    const URL = `${process.env.NEXT_PUBLIC_BASE_URL}/api/chart/all`;
+    const res = await axios.get(URL);
     const data = await res.data;
     return data
   }
